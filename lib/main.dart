ccba3a3a860255b1a0d1032ad487c0fa5d2e1c82
https://github.com/sench32/maxfit <you@example.com>
2020-06-29 19:41:25 +0300

    Add HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,33 @@ class MaxFitApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Max Fitnes',
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(50, 65, 85, 1),
-        textTheme: TextTheme(
-          // ignore: deprecated_member_use
-          title: TextStyle(color: Colors.white),
+        debugShowCheckedModeBanner: false,
+        title: 'Max Fitnes',
+        theme: ThemeData(
+          primaryColor: Color.fromRGBO(50, 65, 85, 1),
+          textTheme: TextTheme(
+            // ignore: deprecated_member_use
+            title: TextStyle(color: Colors.white),
+          ),
         ),
-      ),
-      home: Scaffold(
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text('Max Fit'),
+          leading: Icon(Icons.fitness_center),
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+          child: Text('Page Body'),
         ),
       ),
     );
